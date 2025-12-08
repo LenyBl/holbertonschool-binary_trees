c9191548dde088732396cd5372880b899229ce0b
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-12-08 14:13:14 +0000

    fix: correct parameter documentation in binary_tree_is_leaf function

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,9 +1,8 @@
 #include "binary_trees.h"
 /**
- *
  * binary_tree_is_leaf - Checks if a node is a leaf
  *
- * @param node: Pointer to the node to check
+ * @node: Pointer to the node to check
  *
  * Return: 1 if the node is a leaf, 0 otherwise
  */
