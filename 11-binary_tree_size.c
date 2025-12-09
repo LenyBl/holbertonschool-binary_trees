b9ef39034fd3dff772e8516df041ea9956467f2c
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-12-09 09:44:04 +0000

    fix: adjust function declaration style in binary_tree_size for consistency

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -6,7 +6,8 @@
  *
  * Return: size of the tree, or 0 if tree is NULL
  */
-size_t binary_tree_size(const binary_tree_t *tree){
+size_t binary_tree_size(const binary_tree_t *tree)
+{
 	if (tree == NULL)
 		return (0);
 
