5b27d1790323dd045489642319f53ed04635186b
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-12-09 09:33:42 +0000

    fix: update depth calculation in binary_tree_depth function

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -13,7 +13,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	binary_tree_depth(tree->parent);
+	countDepth = binary_tree_depth(tree->parent);
 
-	return (countDepth++);
+	return (countDepth+1);
 }
