062ed451ad1ae8e01714247d6ab955d6dd0c6bf5
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-12-09 08:43:19 +0000

    fix: correct recursive calls in binary_tree_height function

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -25,7 +25,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	printf("Height form %i: %li", tree->n, count);
 
-	binary_tree_height(tree);
+	binary_tree_height(tree->left);
+	binary_tree_height(tree->right);
 
 	return (0);
 }
