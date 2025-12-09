65b9973b30ca176386cdfa953f4a83e4442794b6
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-12-09 09:28:26 +0000

    feat : create a function that measures the depth of a node in a binary tree

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,11 +8,18 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t countDepth;
+
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (NULL);
-	
-	
+
+	binary_tree_depth(tree->parent);
+	if (tree->parent != NULL)
+	{
+		countDepth++;
+		return (countDepth);
+	}
 }
