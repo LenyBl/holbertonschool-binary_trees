428da99e92da47874f768895d6a28ce7d3e75dfe
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-12-09 14:26:52 +0000

    fix : corriger la logique de vérification d'un arbre binaire complet dans la fonction binary_tree_is_full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,13 +11,13 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL || tree->right != NULL)
+	if (tree->left != NULL && tree->right != NULL)
 	{
-		return (1);
+		return (0);
 	}
 	else
 	{
-		return (0);
+		return (1);
 	}
 	binary_tree_is_full(tree->left);
 	binary_tree_is_full(tree->right);
