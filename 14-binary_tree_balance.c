6f4d642014347baddd0bde39df92637a6951a0f1
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-12-09 13:54:42 +0000

    feat : create a a function that measures the balance factor of a binary tree

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ *
+ * @tree: pointer to the root node of the tree to measure the balance factor
+ *
+ * Return: balance factor of the tree, or 0 if tree is NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int countLeft;
+	int countRight;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL)
+	{
+		countLeft++;
+	}
+	else if (tree->right != NULL)
+	{
+		countRight++;
+	}
+
+	binary_tree_balance(tree->left);
+	binary_tree_balance(tree->right);
+
+	return (countLeft - countRight);
+}
